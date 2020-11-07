fc29517fccc3ee339fb1e4583fbbdd2f2198c871
akelstrom <akelstrom@gmail.com>
2020-11-06 23:28:46 -0500

    set up files, set up styling, set up question objects, set up variables, started writing js functions

---
+++ assets/questions.js
@@ -0,0 +1,42 @@
+var questions = [
+    {
+        "question": "What is this?",
+        "option1": "1",
+        "option2": "2",
+        "option3": "3",
+        "option4": "4",
+        "answer": "2"
+    },
+    {
+        "question": "What is that?",
+        "option1": "1",
+        "option2": "2",
+        "option3": "3",
+        "option4": "4",
+        "answer": "3"
+    },
+    {
+        "question": "Who are you?",
+        "option1": "1",
+        "option2": "2",
+        "option3": "3",
+        "option4": "4",
+        "answer": "3"
+    },
+    {
+        "question": "Who am I?",
+        "option1": "1",
+        "option2": "2",
+        "option3": "3",
+        "option4": "4",
+        "answer": "4"
+    },
+    {
+        "question": "Who am I?",
+        "option1": "1",
+        "option2": "2",
+        "option3": "3",
+        "option4": "4",
+        "answer": "1"
+    }
+]
