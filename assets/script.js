b5dcd35793be47442be86f0a63f2b921da5a703f
akelstrom <akelstrom@gmail.com>
2020-11-08 11:44:18 -0500

    instructions page, start button, countdown function

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,36 @@
 var currentQuestion = 0;
 var score = 0;
-var totalQuestions = questionsArray.length; 
+var totalQuestions = questionsArray.length;
+var startButton = document.querySelector("#start-btn");
+var timerEl = document.querySelector("#countdown");
+var timeLeft = 75;
 
-// answer1El.textContent = questionsArray[0].question
+
+function startScreen() {
+  document.querySelector("#start").innerHTML = `
+    <p> Hello! Welcome to the Coding Quiz! The timer will start when you hit the start button, 
+    and everytime you get a question wrong the timer will deduct 10 seconds.
+    </p>
+    
+    `;
+}
+
+startButton.addEventListener("click", clickStart);
+
+function clickStart() {
+  displayQuestion(0);
+  countDown();
+  //call the timer start function here
+}
 
 //this function displays the questions in the browser, and goes from the first to the second
 function displayQuestion(questionIndex) {
-
-//function within a function that handles what happens when you click the buttons
+  //function within a function that handles what happens when you click the buttons
   function answerClickHandler() {
     if (this.textContent === questionsArray[questionIndex].answer) {
       score += 10;
     } else {
-      //take 10 off timer here
+      timeLeft -= 10
     }
     if (questionIndex + 1 >= totalQuestions) {
       console.log("end");
@@ -20,7 +38,7 @@ function displayQuestion(questionIndex) {
     } else {
       displayQuestion(questionIndex + 1);
     }
-  } 
+  }
   //this query selector selects the parent container div in html, and uses .innerHTML to display the text within, and uses string interprolation `html ${java} html`
   document.querySelector("#question").innerHTML = `
     <h2>${questionsArray[questionIndex].question}</h2>
@@ -45,6 +63,31 @@ function displayQuestion(questionIndex) {
     .addEventListener("click", answerClickHandler);
 }
 
-displayQuestion(0); //change this function to displaystart screen once you make that function
+//change this function to displaystart screen once you make that function
 
-//function to display start screen
+
+//got this function code below from LPS coding bootcamp in-class lesson (11/04/2020)
+function countDown() {
+    // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+    var timeInterval = setInterval(function () {
+      // As long as the `timeLeft` is greater than 1
+      if (timeLeft > 1) {
+        // Set the `textContent` of `timerEl` to show the remaining seconds
+        timerEl.textContent = timeLeft + " seconds remaining";
+        // Decrement `timeLeft` by 1
+        timeLeft--;
+      } else if (timeLeft === 1) {
+        // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
+        timerEl.textContent = timeLeft + " second remaining";
+        timeLeft--;
+      } else {
+        // Once `timeLeft` gets to 0, set `timerEl` to an empty string
+        timerEl.textContent = "";
+        // Use `clearInterval()` to stop the timer
+        clearInterval(timeInterval);
+      }
+    }, 1000);
+  }
+  
+
+startScreen();
