ef94878d740387f26bb48356d1a75ad59dee5f6d
akelstrom <akelstrom@gmail.com>
2020-11-08 18:40:12 -0500

    changed questions

--- assets/questions.js
+++ assets/questions.js
@@ -1,42 +1,42 @@
 var questionsArray = [
     {
-        "question": "What color is the sky?",
-        "option1": "red",
-        "option2": "blue",
-        "option3": "green",
-        "option4": "purple",
-        "answer": "blue"
+        "question": "String values must be enclosed with...?",
+        "option1": "curly brackets",
+        "option2": "quotation marks",
+        "option3": "< >",
+        "option4": "solid brackets",
+        "answer": "quotation marks"
     },
     {
-        "question": "What is that?",
-        "option1": "1",
-        "option2": "2",
-        "option3": "3",
-        "option4": "4",
-        "answer": "3"
+        "question": "Which of the following is the correct way to call a function in JavaScript?",
+        "option1": "functionName();",
+        "option2": "calling functionName",
+        "option3": "functionName{}",
+        "option4": "this is what my function is called",
+        "answer": "functionName();"
     },
     {
-        "question": "Who are you?",
-        "option1": "1",
-        "option2": "2",
-        "option3": "3",
-        "option4": "4",
-        "answer": "3"
+        "question": "What is an array in JavaScript",
+        "option1": "An array is a container object that holds a fixed number of values of a single type",
+        "option2": "Something that performs a task",
+        "option3": "The same thing as a loop",
+        "option4": "Something that comes from the sun",
+        "answer": "An array is a container object that holds a fixed number of values of a single type"
     },
     {
-        "question": "Who am I?",
-        "option1": "1",
-        "option2": "2",
-        "option3": "3",
-        "option4": "4",
-        "answer": "4"
+        "question": "What does document.querySelector do?",
+        "option1": "It responds to an event",
+        "option2": "It adds text to an element",
+        "option3": "It hides elements",
+        "option4": "It returns the element that matches the specified selector",
+        "answer": "It returns the element that matches the specified selector"
     },
     {
-        "question": "How do I finish this?",
-        "option1": "1",
-        "option2": "2",
-        "option3": "3",
-        "option4": "4",
-        "answer": "1"
+        "question": "What is the correct syntax to declare a variable?",
+        "option1": "var nameOfVar = ______",
+        "option2": "the variable nameOfVariable has been declared!",
+        "option3": "varDeclare nameOfVar",
+        "option4": "varResult nameOfVar",
+        "answer": "var nameOfVar = ______"
     }
 ]
